2ca7eeef962798463c42557b8ffbcd6ddd6e7cf2
behrensm03 <behrensm03@gmail.com>
2024-10-02 21:53:29 -0400

    test new route

--- api/index.py
+++ api/index.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+# sample data
+items = [
+  {'id': 1, 'name': 'item 1'},
+  {'id': 2, 'name': 'item 2'}
+]
 
 @app.route('/')
 def home():
@@ -9,3 +15,7 @@ def home():
 @app.route('/about')
 def about():
     return 'About'
+
+@app.route('/items', methods=['GET'])
+def get_items():
+    return jsonify(items)
